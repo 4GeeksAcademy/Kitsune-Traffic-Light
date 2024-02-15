c5a33c86d10397c1400da2042f5094f550cb53ea
Diana PF <149829535+KitsuneDai@users.noreply.github.com>
2024-02-15 20:30:50 +0000

    Finiquitado

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react"
 import Purple from "./Purple";
+ 
 
 
 const Semaforo = () => {
-    const [color, setColor] = useState("none");
+const [color, setColor] = useState("none");
   
-
-    const toggleColor = () => {
-        if (color === "red") setColor("green");
-        else if (color === "green") setColor("yellow");
-        else if (color === "yellow") setColor("red");
-         
+   const changeColor = () => {
+    const colors = ["red", "yellow", "green"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(randomColor);
     };
 
 
@@ -18,18 +17,17 @@ const Semaforo = () => {
     <div className="trafficLight">
         <div className="d-flex">
             <div className= "container mt-5">
+            <Purple/>
                 <div onClick={() => setColor("red")} className= {`redLight ${color === "red" ? "glow" : " "}`}></div> 
                 <div onClick={() => setColor("yellow")} className= {`yellowLight ${color === "yellow" ? "glow" : " "}`}></div>  
                 <div onClick={() => setColor("green")} className= {`greenLight ${color === "green" ? "glow" : " "}`}></div>  
+                
             </div>
-            <div className="remix-button">
-                <button onClick={toggleColor} className="btn btn-primary">Remix</button>
-            </div>
+           
         </div>
-
-     <Purple />
-        
-    
+        <div className="remix-button">
+                <button onClick={changeColor} className="btn btn-primary">Remix</button>
+        </div>
     </div>
     )
 }
